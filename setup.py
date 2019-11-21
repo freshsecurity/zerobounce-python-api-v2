eca72c3268bdb2d8fc98c208320d3bb7b103cf56
joepindar <joepindar@gmail.com>
2019-11-21 15:08:34 +0000

    Patch to remove pypandoc.

--- setup.py
+++ setup.py
@@ -1,21 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from distutils.core import setup
-import pypandoc
+from setuptools import find_packages, setup
+
+with open('README.md', 'r') as fp:
+    README = fp.read()
 
 setup(
     name='zerobounce',
-    version='0.1.4',
+    version='0.1.5',
     description='ZeroBounce Python API - https://www.zerobounce.net.',
     author='Tudor Aursulesei',
     author_email='tudor@zerobounce.net',
     url='http://github.com/zerobounce/zerobounce-python-api',
-    long_description = pypandoc.convert("README.md", "rst"),
-    download_url = 'https://github.com/zerobounce/zerobounce-python-api/archive/0.1.4.tar.gz', # I'll explain this in a second
+    long_description=README,
+    long_description_content_type="text/markdown",
     keywords = ['email', 'validation'], # arbitrary keywords
-    packages=[
-        'zerobounce',
+    download_url='https://github.com/freshsecurity/zerobounce-python-api/dist/0.1.5.tar.gz', # I'll explain this in a second
+    packages=find_packages(),
+    install_requires=[
+        'requests==2.20.0',
     ],
     classifiers=[
         'Development Status :: 4 - Beta',
